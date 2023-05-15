571c42b40a4afa2ccb0874eb522d6b4b54d655d2
Edouard Brodeur <ebrodeur@z3r2p1.42lyon.fr>
2023-05-15 18:50:12 +0200

    RPL_WELCOME done and new user created at connection

--- main.cpp
+++ main.cpp
@@ -16,7 +16,7 @@ int main(int ac, char **av)
 	if (ac == 3)
 	{
 		Server server(atoi(av[1]), av[2]);
-    
+
 		// closing the connected socket
 		close(server.getNewSocket());
 		// closing the listening socket
