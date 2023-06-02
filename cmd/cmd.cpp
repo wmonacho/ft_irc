50b6576f5393ef94cf0b84bc5f1572b0f238cc89
Edouard Brodeur <ebrodeur@z3r2p1.42lyon.fr>
2023-06-02 10:15:26 +0200

    issue with PRIVMSG in channel

--- cmd/cmd.cpp
+++ cmd/cmd.cpp
@@ -553,11 +553,6 @@ bool    cmd::parsePrivmsg(std::string str, Server *server, User *user)
     // Parameters: <msgtarget> <text to be sent>
 	// Exemple : PRIVMSG jreverdy :Are you a frog?
 	std::vector<std::string> arg = splitString(str, " ");
-	if (arg.size() < 3)
-	{
-		std::cerr << "ERR_NOSUCHNICK" << std::endl;
-		return false;
-	}
 	std::string nick_target = arg[1];
 	if (server->nickAlreadyExist(arg[2]) == true)
 	{
@@ -569,15 +564,28 @@ bool    cmd::parsePrivmsg(std::string str, Server *server, User *user)
 		std::cerr << "ERR_NOTEXTTOSEND" << std::endl;
 		return false;
 	}
-	if (!server->nickAlreadyExist(nick_target))
-	{
-		std::cerr << "ERR_NOSUCHNICK" << std::endl;
-		return false;
-	}
-    for(unsigned int i = 0; i < arg.size(); i++)
-    {
-        std::cout << i << " " << arg[i] << std::endl;
+	// if (!server->nickAlreadyExist(nick_target))
+	// {
+	// 	std::cerr << "ERR_NOSUCHNICK" << std::endl;
+	// 	return false;
+	// }
+
+    // utiliser arg pour trouver le channel --> le recup et envoyer le message a tout les utilisateurs dedans
+    if (arg[1].find("#") != std::string::npos) {
+        std::cout << "ENTER CHANNEL USAGE" << std::endl;
+        arg[1].erase(0, 1);
+        std::cout << "CHANNEL ==> " << arg[1] << std::endl;
+        if (server->channelAlreadyExist(arg[1])) {
+            std::cout << "Channel exists so we can send to all users = " << arg[1] << std::endl; 
+            Channel *channel = server->getChannel(arg[1]);
+            arg[1].insert(0, "#");
+            std::string message = ":" + user->getNickname() + "!" + user->getUsername() + "@locahost " + arg[0] + " " + arg[1] + " " + arg[2] + "\r\n";
+            sendResponseToAllUsersInChannel(message, channel);
+            return true ;
+        }
+        std::cerr << "No channel found" << std::endl;
     }
+
     User *dest = server->getUser(nick_target);
     std::string message = ":" + user->getNickname() + "!" + user->getUsername() + "@locahost " + arg[0] + " " + arg[1] + " " + arg[2] + "\r\n";
     send(dest->getSocket(), message.c_str(), message.size(), 0);
