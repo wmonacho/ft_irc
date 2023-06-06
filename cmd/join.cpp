fcab8a31a7c515c70441603587c3c31ec6114d2e
Edouard Brodeur <ebrodeur@z1r8p1.42lyon.fr>
2023-06-06 16:31:31 +0200

    Started to implement send to all commands

--- cmd/join.cpp
+++ cmd/join.cpp
@@ -2,11 +2,10 @@
 
 bool    cmd::parseJoin(std::string str, Server *server, User *user)
 {
-    // std::cout << "str: " << str << std::endl;
+    // Parsing de la string (commande + argument )
     std::vector<std::string> splitArg = splitString(str, " ");
     if (splitArg.size() != 2)
 	 return false;
-    //check si pas de # devant le channel (jwe crois que l'on peut mettre & aussi a verifier)
     if (splitArg[1][0] != '#')
     {
 	 std::cerr << "ERR_BADCHANNELKEY" << std::endl;
@@ -14,27 +13,44 @@ bool    cmd::parseJoin(std::string str, Server *server, User *user)
     }
     std::string channel_name= &splitArg[1][1];
     std::string server_response = createServerMessage(user, "", splitArg);
+
+    // Cas 1 : le channel existe, donc y ajoute le user et on envoie le message de "bienvenue" a tout le monde
+    // + on envoie le topic (s'il existe) et la liste des USER 
     if (server->channelAlreadyExist(channel_name))
 	{
 	    Channel* channel = server->getChannel(channel_name);
-        std::cout << "Joining --> " << channel->getName() << std::endl;
  		UserAspects	new_aspects(0);
-		// channel->setUserList(user, new_aspects);
         server->addUserToChannel(channel_name, user, new_aspects);
-        // We send a message to all the users connected to the channel
-        sendResponseToAllUsersInChannel(server_response, channel);
-        sendChannelTopicToUser(channel, user);
+        sendMessageToAllUsersInChannel(server_response, channel);
+        // topic s'il existe
+        // liste des users
+        if (channel->getUserList().size() > 1) {
+            std::string user_list = std::string(":localhost ") + "353" + " " + user->getUsername() + " == #" + channel->getName() + ":";
+            std::map<const User*, UserAspects> userMap = channel->getUserList();
+            std::map<const User*, UserAspects>::iterator userNode = userMap.begin();
+            std::map<const User*, UserAspects>::iterator lastUserNode = userMap.end();
+
+            while (userNode != lastUserNode) {
+                user_list.append(userNode->first->getUsername());
+                user_list.append(" ");
+                userNode++;
+            }
+            user_list.append("\r\n");
+            std::cout << "USER_LIST --> " << user_list;
+            send(user->getSocket(), user_list.c_str(), user_list.size(), 0);
+        }
 		return true;
 	}
-    /*snon creer un nouveau Channel y ajouter le User avec les droits admin et utiliser setNewChannelInMap ensuite*/
+
+    // Cas 2 : le channel n'existe pas, il faut donc le creer dans notre serveur et y ajouter l'utilisateur
     UserAspects	new_aspects(1);
     Channel *channel = new Channel(channel_name);
     server->createNewChannel(channel_name, channel);
     if (!server->getChannel(channel_name))
 	 return false;
     server->addUserToChannel(channel_name, user, new_aspects);
-    // After the channel creation (if it didn't exist)
-    sendResponseToAllUsersInChannel(server_response, server->getChannel(channel_name));
+    sendMessageToAllUsersInChannel(server_response, server->getChannel(channel_name));
+    //sendChannelTopicToUser(server->getChannel(channel_name), user);
     return true;
 }
 
--- cmd/privmsg.cpp
+++ cmd/privmsg.cpp
@@ -2,6 +2,7 @@
 
 bool    cmd::parsePrivmsg(std::string str, Server *server, User *user)
 {
+	// parsing de l'input user
 	std::vector<std::string> arg = splitString(str, " ");
 	std::string nick_target = arg[1];
 	if (server->nickAlreadyExist(arg[2]) == true)
@@ -20,32 +21,26 @@ bool    cmd::parsePrivmsg(std::string str, Server *server, User *user)
 	// 	return false;
 	// }
 
-    // utiliser arg pour trouver le channel --> le recup et envoyer le message a tout les utilisateurs dedans
+    // Cas 1 : le user parle dans un channel
     if (arg[1].find("#") != std::string::npos) {
-        std::cout << "ENTER CHANNEL USAGE" << std::endl;
         arg[1].erase(0, 1);
-        std::cout << "CHANNEL ==> " << arg[1] << std::endl;
         if (server->channelAlreadyExist(arg[1])) {
             std::cout << "Channel exists so we can send to all users = " << arg[1] << std::endl; 
             Channel *channel = server->getChannel(arg[1]);
             arg[1].insert(0, "#");
-
+			rebuildMessage(arg, 2);
             std::string message = ":" + user->getNickname() + "!" + user->getUsername() + "@locahost " + arg[0] + " " + arg[1] + " " + arg[2] + "\r\n";
 			std::cout << "======> " << message << std::endl;
-            sendResponseToAllUsersInChannel(message, channel);
+			sendMessageToOtherUsersInChannel(message, channel, user);
             return true ;
         }
         std::cerr << "No channel found" << std::endl;
+		return false;
     }
 
+	// Cas 2 : le user parle directement a un autre user
     User *dest = server->getUser(nick_target);
-    size_t i = 3;
-	while (i < arg.size()) {
-		arg[2].append(" ");
-		arg[2].append(arg[i]);
-		i++;
-	}
-	arg[2].append("\0");
+	rebuildMessage(arg, 2);
     std::string message = ":" + user->getNickname() + "!" + user->getUsername() + "@locahost " + arg[0] + " " + arg[1] + " " + arg[2] + "\r\n";
     send(dest->getSocket(), message.c_str(), message.size(), 0);
 	return true;
