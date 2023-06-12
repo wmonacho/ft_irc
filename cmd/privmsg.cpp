e4200f1f083914225b7cdf45d1de4e81f56e9625
Edouard Brodeur <ebrodeur@z3r2p1.42lyon.fr>
2023-06-12 15:47:12 +0200

    Implemented all numeric replies for this command

--- cmd/privmsg.cpp
+++ cmd/privmsg.cpp
@@ -4,29 +4,70 @@ bool    cmd::parsePrivmsg(std::string str, Server *server, User *user)
 {
 	// parsing de l'input user
 	std::vector<std::string> arg = splitString(str, " ");
-	std::string nick_target = arg[1];
-	if (arg.size() > 3)
-	{
-		// attention car le message est pas rebuild donc a plusieurs argments
-		// 407	ERR_TOOMANYTARGETS
-		std::string error = generateErrorMessage("407", arg[0]);
-		std::cout << error << std::endl;
-		send(user->getSocket(), error.c_str(), error.size(), 0);
+	std::string target = arg[1];
+	int	sizeWithoutMessage;
+	std::vector<std::string>::iterator messagePos;
+	std::cout << "ARG SIZE ==> " << arg.size() << std::endl;
+
+	if (arg.size() < 3) {
+		std::cerr << "Error: missing paramaters for PRIVMSG command" << std::endl;
 		return false;
 	}
-	if (arg[2].empty())
-	{
-		std::cerr << "ERROR 2" << std::endl;
+
+	int	message_check = 0;
+	int	number_of_words = 0;
+	int	number_of_recipients = 0;
+	for (std::vector<std::string>::iterator it = arg.begin(); it != arg.end(); it++) {
+		// On trouve ":" qui signale le debut du message
+		if (it->find(":") != std::string::npos) {
+			message_check++;
+			messagePos = it;
+		}
+		// On compte le nombre de mots dans le message
+		if (message_check > 0) {
+			number_of_words++;
+		}
+	}
+	if (message_check != 1) {
 		// 412	ERR_NOTEXTTOSEND
-		std::string error = generateErrorMessage("412", arg[0]);
+		//std::string error = generateErrorMessage("412", arg[0]);
+		std::string error = std::string(":localhost ") + "412" + " " + arg[0] + " :No text to send" + "\r\n";
+		std::cerr << "412	ERR_NOTEXTTOSEND --> " << message_check << " // " << error << std::endl;	// DEBUG
 		send(user->getSocket(), error.c_str(), error.size(), 0);
 		return false;
 	}
-	// if (!server->nickAlreadyExist(nick_target))
-	// {
-	// 	std::cerr << "ERR_NOSUCHNICK" << std::endl;
-	// 	return false;
-	// }
+
+	std::cout << "Number of words --> " << number_of_words << std::endl;
+	sizeWithoutMessage = arg.size() - number_of_words;
+	std::cout << "sizeWithoutMessage --> " << sizeWithoutMessage << std::endl;
+	if (message_check == 1 && sizeWithoutMessage == 1) {
+	    // 411	ERR_NORECIPIENT ":No recipient given (<command>)"
+		std::string error = std::string(":localhost ") + "411" + " " + arg[0] + " :No recipient given " + "\r\n";
+		std::cerr << "411	ERR_NORECIPIENT --> " << error << std::endl;	// DEBUG
+		send(user->getSocket(), error.c_str(), error.size(), 0);
+		return false;
+	}
+
+	// 407	ERR_TOOMANYTARGETS "<target> :Duplicate recipients. No message \/ delivered"
+	for (std::vector<std::string>::iterator it = arg.begin(); it != messagePos; it++) {
+		number_of_recipients++;
+	}
+	int i = 1;
+	int j;
+	while (i < number_of_recipients) {
+		j = 1;
+		while (j < number_of_recipients) {
+			if (arg[i] == arg[j] && i != j) {
+				// 407	ERR_TOOMANYTARGETS "<target> :Duplicate recipients. No message \/ delivered"
+				std::string error = std::string(":localhost ") + "407" + " " + arg[0] + " " + arg[i] + " :Duplicate recipients. No message delivered" + "\r\n";
+				std::cerr << "407	ERR_NORECIPIENT --> " << error << std::endl;	// DEBUG
+				send(user->getSocket(), error.c_str(), error.size(), 0);
+				return false;
+			}
+			j++;
+		}
+		i++;
+	}
 
     // Cas 1 : le user parle dans un channel
     if (arg[1].find("#") != std::string::npos) {
@@ -40,21 +81,27 @@ bool    cmd::parsePrivmsg(std::string str, Server *server, User *user)
             return true ;
         }
 		// 404	ERR_CANNOTSENDTOCHAN
-		std::string error = generateErrorMessage("404", arg[0]);
+		//std::string error = generateErrorMessage("404", arg[0]);
+		std::string error = std::string(":localhost ") + "404" + " " + arg[0] + " " + arg[1] + " :Cannot send to channel" + "\r\n";
+		std::cerr << "404	ERR_CANNOTSENDTOCHAN --> " << error << std::endl;	// DEBUG
 		send(user->getSocket(), error.c_str(), error.size(), 0);
 		return false;
     }
 
 	// Cas 2 : le user parle directement a un autre user
-    User *dest = server->getUser(nick_target);
-	// 411 ERR_NORECIPIENT
+    User *dest = server->getUser(target);
 	if (!dest) {
-		std::string error = generateErrorMessage("411", arg[0]);
+		// 401	ERR_NOSUCHNICK "<nickname> :No such nick/channel"
+		//std::string error = generateErrorMessage("401", arg[0]);
+		std::string error = std::string(":localhost ") + "401" + " " + arg[0] + " " + arg[1] + " :No such nick" + "\r\n";
+		std::cerr << "401	ERR_NOSUCHNICK --> " << error << std::endl;	// DEBUG
 		send(user->getSocket(), error.c_str(), error.size(), 0);
 		return false;
 	}
 	rebuildMessage(arg, 2);
     std::string message = ":" + user->getNickname() + "!" + user->getUsername() + "@locahost " + arg[0] + " " + arg[1] + " " + arg[2] + "\r\n";
-    send(dest->getSocket(), message.c_str(), message.size(), 0);
+    std::string rpl_away = std::string(":localhost ") + "401" + " " + arg[0] + " " + user->getNickname() + " :Message sent" + "\r\n";
+	send(dest->getSocket(), message.c_str(), message.size(), 0);
+	send(user->getSocket(), rpl_away.c_str(), rpl_away.size(), 0);
 	return true;
 }
