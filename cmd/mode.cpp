f549707532b9fdc7cf121deb26b39b7a8239bdf8
WMonach <mimimonach@gmail.com>
2023-06-12 17:19:32 +0200

    add numeric replies for MODE cmd and change of testbranch

--- cmd/mode.cpp
+++ cmd/mode.cpp
@@ -7,31 +7,40 @@ bool    cmd::parseMode(std::string str, Server *server, User *user)
 	//MODE <cible> <mode> <argument(s)>
     if (splitArg.size() < 3)
     {
-        //envoyer numeric replies
-        std::cerr << "MODE: need more params " << std::endl;
+        // 461  ERR_NEEDMOREPARAMS
+        std::string error = generateErrorMessage("461", splitArg[0]);
+		send(user->getSocket(), error.c_str(), error.size(), 0);
         return (false);
     }
     //check si # devant la cible et si le channel existe
 	if ((splitArg[1][0] != '#' && splitArg[1][0] != '&') || server->getMap().find(&splitArg[1][1]) == server->getMap().end())
 	{
-           std::cerr << "MODE: can't find this channel " << std::endl;
            return (false);
 	}
 	//check si le User est bien dans la userlist du channel
 	if (!server->userInChannel(&splitArg[1][1], user))
-           return false;
+	{
+			// 441 ERR_USERNOTINCHANNEL
+			std::string error = generateErrorMessage("441", splitArg[0]);
+			send(user->getSocket(), error.c_str(), error.size(), 0);
+        	return false;
+	}
     //check si + ou - devant le mode
 	if ((splitArg[2][0] != '-' && splitArg[2][0] != '+') || splitArg[2].size() != 2)
 	{
-           std::cerr << "MODE: no chan modes " << std::endl;
-           return (false);
+			// 477 ERR_NOCHANMODES
+        	std::string error = generateErrorMessage("477", splitArg[0]);
+			send(user->getSocket(), error.c_str(), error.size(), 0);
+        	return (false);
 	}
     //check si le mode existe (tout depend de ceux que l'on prend)
 	std::string modes = "iktlo";
 	if (modes.find(&splitArg[2][1]) == std::string::npos)
 	{
-           std::cerr << "MODE: unknow mode " << std::endl;
-           return (false);
+        	// 472 ERR_UNKNOWNMODE
+			std::string error = generateErrorMessage("472", splitArg[0]);
+			send(user->getSocket(), error.c_str(), error.size(), 0);
+        	return (false);
 	}
     Channel *chan = server->getChannel(&splitArg[1][1]);
     const User* u = server->getChannelUser(&splitArg[1][1], splitArg[3]);
@@ -49,6 +58,11 @@ bool    cmd::parseMode(std::string str, Server *server, User *user)
                     	break;
                   case 107:
                     	//execute mode k
+						if (chan->getPassword() != "") {
+							// 467 ERR_KEYSET
+							std::string error = generateErrorMessage("467", splitArg[0]);
+							send(user->getSocket(), error.c_str(), error.size(), 0);
+						}
                     	chan->setPassword(splitArg[3]);
                     	break;
                   case 108:
@@ -93,3 +107,14 @@ bool    cmd::parseMode(std::string str, Server *server, User *user)
 	}
 	return (true);
 }
+
+// Numeric Replies:
+
+//
+//                           ERR_CHANOPRIVSNEEDED a voir
+//                       
+//           RPL_CHANNELMODEIS
+//           RPL_BANLIST                     RPL_ENDOFBANLIST
+//           RPL_EXCEPTLIST                  RPL_ENDOFEXCEPTLIST
+//           RPL_INVITELIST                  RPL_ENDOFINVITELIST
+//           RPL_UNIQOPIS
