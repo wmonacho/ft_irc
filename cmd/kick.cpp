17bae37fbbbdf73b472542e67bfada426a8e9f3b
Edouard Brodeur <ebrodeur@z3r2p1.42lyon.fr>
2023-06-16 11:48:41 +0200

    tried to fix name_list in JOIN + convert indent to tab in all /cmd files

--- cmd/kick.cpp
+++ cmd/kick.cpp
@@ -1,39 +1,39 @@
 #include "cmd.hpp"
 
-bool    cmd::parseKick(std::string str, Server *server, User *user)
+bool	cmd::parseKick(std::string str, Server *server, User *user)
 {
 	// Parsing de l'input du user
 	// faire un check de channel si en arg[1] ou si en arg[2] si aucun return false
-    std::vector<std::string> arg = splitString(str, " ");
-    if (arg.size() < 3)
-    {
-        // 461 ERR_NEEDMOREPARAMS
-        std::string error = std::string("localhost :") + "461 " + user->getNickname() + " " + arg[0] + " :Not enough parameters" + "\r\n";
-        send(user->getSocket(), error.c_str(), error.size(), 0);
-	    return false;
-    }
-    if (arg[2].find("#") == std::string::npos && arg[2].find("&") == std::string::npos)
-    {
+	std::vector<std::string> arg = splitString(str, " ");
+	if (arg.size() < 3)
+	{
+		// 461 ERR_NEEDMOREPARAMS
+		std::string error = std::string("localhost :") + "461 " + user->getNickname() + " " + arg[0] + " :Not enough parameters" + "\r\n";
+		send(user->getSocket(), error.c_str(), error.size(), 0);
+		return false;
+	}
+	if (arg[2].find("#") == std::string::npos && arg[2].find("&") == std::string::npos)
+	{
 		// 476 ERR_BADMASKCHANNEL
 		std::string error = std::string("localhost :") + "476 " + user->getNickname() + " " + arg[2] + " :Bad Channel Mask" + "\r\n";
-        send(user->getSocket(), error.c_str(), error.size(), 0);
-	    return false;
-    }
-    if (!server->channelAlreadyExist(&arg[2][1]))
-    {
+		send(user->getSocket(), error.c_str(), error.size(), 0);
+		return false;
+	}
+	if (!server->channelAlreadyExist(&arg[2][1]))
+	{
 		// 403 ERR_NOSUCHCHANNEL
-        std::string error = std::string("localhost :") + "403 " + user->getNickname() +  " :No such channel" + "\r\n";
-        return false;
-    }
-    if (!server->userInChannel(&arg[2][1], server->getChannelUser(&arg[2][1], &arg[3][1])))
+		std::string error = std::string("localhost :") + "403 " + user->getNickname() +  " :No such channel" + "\r\n";
+		return false;
+	}
+	if (!server->userInChannel(&arg[2][1], server->getChannelUser(&arg[2][1], &arg[3][1])))
 	{
 		// 441 ERR_USERNOTINCHANNEL
 		std::string error = std::string(":localhost ") + "441 " + user->getNickname() + " " + arg[2] + " :They aren't on that channel" + "\r\n";
 		send(user->getSocket(), error.c_str(), error.size(), 0);
-        return false;
+		return false;
 	}
 	
-    // Cas 1 : il y a un commentaire au KICK
+	// Cas 1 : il y a un commentaire au KICK
 	server->kickUserFromChannel(&arg[2][1], server->getUser(&arg[3][1]));
 	if (arg.size() == 5)
 	{
@@ -53,7 +53,7 @@ bool    cmd::parseKick(std::string str, Server *server, User *user)
 	std::string kick_message = ":" + user->getNickname() + "!" + user->getUsername() + "@locahost " + arg[0] + " " + arg[2] + " " + &arg[3][1] + " :You are KICK man" + "\r\n";
 	std::cout << kick_message << std::endl;
 	sendMessageToAllUsersInChannel(kick_message, server->getChannel(&arg[2][1]));
-    return true;
+	return true;
 }
 
 /*---------------------------------------------*/
--- cmd/list.cpp
+++ cmd/list.cpp
@@ -1,55 +1,55 @@
 #include "cmd.hpp"
 
-bool    cmd::parseList(std::string str, Server *server, User *user)
+bool	cmd::parseList(std::string str, Server *server, User *user)
 {
-    // Parsing user input
-    std::vector<std::string> arg = splitString(str, " ");
-    std::map<std::string, Channel*> map = server->getMap();
-    std::vector<std::string> channels;
+	// Parsing user input
+	std::vector<std::string> arg = splitString(str, " ");
+	std::map<std::string, Channel*> map = server->getMap();
+	std::vector<std::string> channels;
 
-    if (map.empty()) {
-        std::string list_start = std::string(":localhost ") + "321" + " " + arg[0] + "\r\n";
-        std::string list_end = std::string(":localhost ") + "323" + " " + arg[0] + "\r\n";
-        send(user->getSocket(), list_start.c_str(), list_start.size(), 0);
-        send(user->getSocket(), list_end.c_str(), list_end.size(), 0);
-    }
+	if (map.empty()) {
+		std::string list_start = std::string(":localhost ") + "321" + " " + arg[0] + "\r\n";
+		std::string list_end = std::string(":localhost ") + "323" + " " + arg[0] + "\r\n";
+		send(user->getSocket(), list_start.c_str(), list_start.size(), 0);
+		send(user->getSocket(), list_end.c_str(), list_end.size(), 0);
+	}
 
-    if (arg.size() == 1)
-    {
-        std::string channels_list;
-        for (std::map<std::string, Channel*>::iterator it = map.begin(); it != map.end(); it++)
-        {
-            channels_list.append(it->second->getName());
-            channels_list.append(" ");
-        }
-        std::string list_message = std::string(":localhost ") + "321" + " " + arg[0] + "\r\n";
-        send(user->getSocket(), list_message.c_str(), list_message.size(), 0);
-        return true;
-    }
-    arg.erase(arg.begin());
-    channels = splitString(arg[1], ",");
-    for (std::vector<std::string>::iterator it = arg.begin(); it != arg.end(); it++)
-    {
-        std::map<std::string, Channel*>::iterator itMap = map.begin();
-        int isValid = 0;
-        while (itMap != map.end())
-        {
-            if (itMap->second->getName() == *it)
-            {
-                std::cout << "Channel: " << itMap->second->getName() << std::endl;
-                std::cout << " - topic: " << itMap->second->getTopic() << std::endl;
-                isValid = 1;
-            }
-            else
-                itMap++;
-        }
-        if (isValid == 0)
-        {
-            std::cerr << "Channel: " << itMap->second->getName() << "doesn't exist."  << std::endl;
-            return false;
-        }
-    }
-    return true;
+	if (arg.size() == 1)
+	{
+		std::string channels_list;
+		for (std::map<std::string, Channel*>::iterator it = map.begin(); it != map.end(); it++)
+		{
+			channels_list.append(it->second->getName());
+			channels_list.append(" ");
+		}
+		std::string list_message = std::string(":localhost ") + "321" + " " + arg[0] + "\r\n";
+		send(user->getSocket(), list_message.c_str(), list_message.size(), 0);
+		return true;
+	}
+	arg.erase(arg.begin());
+	channels = splitString(arg[1], ",");
+	for (std::vector<std::string>::iterator it = arg.begin(); it != arg.end(); it++)
+	{
+		std::map<std::string, Channel*>::iterator itMap = map.begin();
+		int isValid = 0;
+		while (itMap != map.end())
+		{
+			if (itMap->second->getName() == *it)
+			{
+				std::cout << "Channel: " << itMap->second->getName() << std::endl;
+				std::cout << " - topic: " << itMap->second->getTopic() << std::endl;
+				isValid = 1;
+			}
+			else
+				itMap++;
+		}
+		if (isValid == 0)
+		{
+			std::cerr << "Channel: " << itMap->second->getName() << "doesn't exist."  << std::endl;
+			return false;
+		}
+	}
+	return true;
 }
 
 // /list = on liste tout les channels et leur topic
