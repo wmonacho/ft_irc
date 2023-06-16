74b53f963d8905d5b6f77bff2bdc150d5ac0751b
William Monachon <86129877+wmonacho@users.noreply.github.com>
2023-06-16 13:41:24 +0200

    Debug branch (#91)
    * Test branch (#73)
    
    * nothing
    
    * repair Users and channel
    
    * trying to repair kickuser
    
    * repair erase for all
    
    * resolve crash for MODE cmd
    
    * add numeric replies for MODE cmd and change of testbranch
    
    * numeric rpl mode
    
    * finish join cmd, have to finish RPL on MODE cmd
    
    * resolve conflit
    
    * repair JOIN
    
    * limit check
    
    * rpl channel_mode is
    
    * kick cmd
    
    * kikick
    
    * try to set JOIN_LIST
    
    * kick cmd maj, approx
    
    * joinjoin
    
    * conflit
    
    * kick message kicker

--- cmd/kick.cpp
+++ cmd/kick.cpp
@@ -4,16 +4,16 @@ bool	cmd::parseKick(std::string str, Server *server, User *user)
 {
 	// Parsing de l'input du user
 	// faire un check de channel si en arg[1] ou si en arg[2] si aucun return false
-	std::vector<std::string> arg = splitString(str, " ");
-	if (arg.size() < 3)
-	{
-		// 461 ERR_NEEDMOREPARAMS
-		std::string error = std::string("localhost :") + "461 " + user->getNickname() + " " + arg[0] + " :Not enough parameters" + "\r\n";
-		send(user->getSocket(), error.c_str(), error.size(), 0);
-		return false;
-	}
-	if (arg[2].find("#") == std::string::npos && arg[2].find("&") == std::string::npos)
-	{
+    std::vector<std::string> arg = splitString(str, " ");
+    if (arg.size() < 3)
+    {
+        // 461 ERR_NEEDMOREPARAMS
+        std::string error = std::string("localhost :") + "461 " + user->getNickname() + " " + arg[0] + " :Not enough parameters" + "\r\n";
+        send(user->getSocket(), error.c_str(), error.size(), 0);
+	    return false;
+    }
+    if (arg[2][0] != '#' && arg[2][0] != '&')
+    {
 		// 476 ERR_BADMASKCHANNEL
 		std::string error = std::string("localhost :") + "476 " + user->getNickname() + " " + arg[2] + " :Bad Channel Mask" + "\r\n";
 		send(user->getSocket(), error.c_str(), error.size(), 0);
@@ -35,7 +35,7 @@ bool	cmd::parseKick(std::string str, Server *server, User *user)
 	
 	// Cas 1 : il y a un commentaire au KICK
 	server->kickUserFromChannel(&arg[2][1], server->getUser(&arg[3][1]));
-	if (arg.size() == 5)
+	if (arg.size() > 4)
 	{
 		if (arg[4][0] != ':')
 		{
@@ -43,17 +43,25 @@ bool	cmd::parseKick(std::string str, Server *server, User *user)
 			return false;
 		}
 		rebuildMessage(arg, 3);
-		std::string kick_comment = arg[4];
-		std::string kick_message = ":" + user->getNickname() + "!" + user->getUsername() + "@locahost " + arg[0] + " " + arg[2] + " " + &arg[3][1] + kick_comment + "\r\n";
+		std::string kick_comment;
+		size_t i = 4;
+		while (i < arg.size() - 1) {
+			kick_comment += arg[i] + " ";
+			i++;
+		}
+		kick_comment += arg[i];
+		std::string kick_message = ":" + user->getNickname() + "!" + user->getUsername() + "@locahost " + arg[0] + " " + arg[2] + " " + arg[3] + kick_comment + "\r\n";
 		sendMessageToAllUsersInChannel(kick_message, server->getChannel(&arg[2][1]));
 		return true;
 	}
 
 	// Cas 2 : pas de commentaire au KICK
-	std::string kick_message = ":" + user->getNickname() + "!" + user->getUsername() + "@locahost " + arg[0] + " " + arg[2] + " " + &arg[3][1] + " :You are KICK man" + "\r\n";
+	std::string kick_message = ":" + user->getNickname() + "!" + user->getUsername() + "@locahost " + arg[0] + " " + arg[2] + " " + arg[3] + " You are KICK man" + "\r\n";
 	std::cout << kick_message << std::endl;
 	sendMessageToAllUsersInChannel(kick_message, server->getChannel(&arg[2][1]));
-	return true;
+	kick_message = ":" + user->getNickname() + "!" + user->getUsername() + "@locahost " + arg[0] + " " + arg[2] + " " + arg[3] + " You are KICK man" + "\r\n";
+	send(server->getUser(&arg[3][1])->getSocket(), kick_message.c_str(), kick_message.size(), 0);
+    return true;
 }
 
 /*---------------------------------------------*/
