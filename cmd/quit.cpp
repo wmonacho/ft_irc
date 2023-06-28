ecaeffa8012e05584c092a4d39dc7cde80b3799a
Edouard Brodeur <ebrodeur@z3r2p5.42lyon.fr>
2023-06-28 19:53:39 +0200

    Fixed closed connection with recv

--- cmd/quit.cpp
+++ cmd/quit.cpp
@@ -7,9 +7,12 @@ bool	cmd::parseQuit(Server *server, std::string str, User * user)
 		return false;
 	}
 
+	if (user)
+		std::cerr << "The user [" << user->getNickname() << "] is leaving the server, he will be erased" << std::endl;
 	this->parseJoin("JOIN 0", server, user);
 	if (user) {
 		server->deleteUserFromUserList(*user);
+		std::cerr << "the user has been deleted from the database" << std::endl;
 	}
 	return true;
 }
